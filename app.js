161dfabb0f4f252dcc5084c9f29c6d546b37df2d
calvin <119773802+calvindany@users.noreply.github.com>
2024-05-18 22:56:49 +0700

    added: 404 controller

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+const errorController = require('./controller/errorController');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -16,8 +18,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found<h1>')
-})
+app.use(errorController.get404)
 
 app.listen(3000);
