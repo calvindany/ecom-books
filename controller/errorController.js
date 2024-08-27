44c0fe58dc1c8f2b4d125fc8b28236f0852e1c05
Calvin Danyalson <119773802+calvindany@users.noreply.github.com>
2024-08-27 23:43:15 +0700

    fixed: page not found hardcoded page

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,3 +1,9 @@
 exports.get404 = (req, res, next) => {
-    res.status(404).send('<h1>Page Not Found<h1>')
+    // res.status(404).send('<h1>Page Not Found<h1>')
+
+    res.render('404', {
+        pageTitle: 'Page Not Found!',
+        path: '/not-found'
+
+    })
 };
